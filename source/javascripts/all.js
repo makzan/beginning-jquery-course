af987125cd6d01a057867145cf97bc7d966b9b4a
Thomas Seng Hin Mak <mak@makzan.net>
2016-03-05 09:16:08 +0000

    WIP, adding user auth.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,6 @@
 //= require_tree .
+
+
+$('#error-message').click(function(){
+  $(this).text('');
+});
--- source/javascripts/auth.js
+++ source/javascripts/auth.js
@@ -1,4 +1,6 @@
 (function(){
-  //var ref = new Firebase('https://teaching.firebaseio.com/jQuery/mentor-chats/' + emailSlug + '/');
-  //var user = ref.getAuth();
+  var authRef = new Firebase('https://teaching.firebaseio.com/');
+  window.user = authRef.getAuth();
+
+
 })();
