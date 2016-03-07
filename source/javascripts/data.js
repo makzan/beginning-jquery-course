73323ead5eeff86a6883df64c6d75810e481b72b
Thomas Seng Hin Mak <mak@makzan.net>
2016-03-07 05:12:17 +0000

    Change to use intercom chat.

--- source/javascripts/data.js
+++ source/javascripts/data.js
@@ -4,19 +4,40 @@
 
   window.userInfo = undefined;
 
+  window.setUserInfo = function(name, email, uid) {
+    window.userInfo = window.userInfo || {};
+    if (name !== null) window.userInfo.displayName = name;
+    if (email !== null) window.userInfo.email = email;
+    if (uid !== null) window.userInfo.uid = uid;
+
+    localStorage["userInfo"] = JSON.stringify(userInfo);
+
+    var userInfoRef = new Firebase(global.fburl + 'users/' + userInfo.uid);
+    userInfoRef.set(window.userInfo);
+  };
+  window.getUserInfo = function() {
+    var userInfoRef = new Firebase(global.fburl + 'users/' + user.uid);
+    userInfoRef.on('value', function(snapshot){
+      userInfo = snapshot.val();
+      userInfo.uid = user.uid;
+
+      console.log("Fetched userInfo:", userInfo);
+
+      localStorage["userInfo"] = JSON.stringify(userInfo);
+
+    });
+  }
+
+
   // read user info
   if (localStorage["userInfo"] !== undefined) {
     userInfo = JSON.parse(localStorage["userInfo"]);
   }
 
   if (user !== null) {
-    var userInfoRef = new Firebase(global.fburl + 'users/' + user.uid);
-    userInfoRef.on('value', function(snapshot){
-      userInfo = snapshot.val();
-
-      localStorage["userInfo"] = JSON.stringify(userInfo);
-
-    });
+    getUserInfo();
   }
+
+
 
 })();
