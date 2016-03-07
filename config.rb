8c04e5f555efc828de10b5d061ad3a046ae12dda
Thomas Seng Hin Mak <mak@makzan.net>
2016-03-07 09:01:35 +0000

    Change URL structure to hide 'cards/'

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "cards/{cid}-{title}.html"
+  blog.permalink = "{cid}-{title}.html"
   # Matcher for blog source files
   blog.sources = "cards/{cid}-{title}.html"
   blog.taglink = "tags/{tag}.html"
