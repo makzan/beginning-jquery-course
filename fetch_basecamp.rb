687270fb92296b767a175a2353553939ce0d6a82
Thomas Seng Hin Mak <mak@makzan.net>
2016-04-10 06:44:31 +0000

    Almost finish fetch_basecamp.rb.

--- fetch_basecamp.rb
+++ fetch_basecamp.rb
@@ -2,12 +2,23 @@ require 'rubygems'
 require 'nokogiri'
 require 'rest_client'
 
-page = Nokogiri::HTML(RestClient.get("https://public.3.basecamp.com/p/28d3eaTmLNfacdzsMkZZMsX8"))
+url = "https://public.3.basecamp.com/p/x5oZm9qSiXDJnhN8pdcjFfZe"
 
-title = page.css("h2").to_s.gsub(/<\/h2>/,"").gsub(/<h2[^>]*>/,"")
+page = Nokogiri::HTML(RestClient.get(url))
 
-content = page.css(".formatted_content > div").to_s
-content = content.gsub(/<br>/,"\n").gsub(/<\/?div>/,"").gsub(/<\/a>/,"").gsub(/<a[^>]*>/,"")
+links = page.css(".formatted_content a").to_s.gsub(/<a[^>]*>/,"").split("</a>")
 
-puts title
-puts content
+links.each do |link|
+  puts "Start process: #{link}"
+  page = Nokogiri::HTML(RestClient.get(link))
+  title = page.css("h2").to_s.gsub(/<\/h2>/,"").gsub(/<h2[^>]*>/,"")
+
+  content = page.css(".formatted_content > div").to_s
+  content = content.gsub(/<br>/,"\n").gsub(/<\/?div>/,"").gsub(/<\/a>/,"").gsub(/<a[^>]*>/,"").gsub("&lt;", "<").gsub("&gt;",">")
+
+  # puts title
+  # puts content
+
+  File.write('source/cards/' + title + '.md', content)
+  puts "#{title}.md is written to file."
+end
